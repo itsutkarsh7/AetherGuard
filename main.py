d35fb8eafb5a8099ac8e355082e927d5bb0d3780
Utkarsh Singh <your-email@example.com>
2025-08-04 08:13:07 +0530

    Fix run file and Procfile for Render

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-from main import create_app
+from sentinelAI import create_app  # Replace with your actual app package name
 
 app = create_app()
+
+if __name__ == "__main__":
+    app.run(debug=True)
