5ae3de684ec910a65b4302967e456b7204a781fe
Utkarsh Singh <your-email@example.com>
2025-08-06 16:52:47 +0530

    Fixs

--- app/__init__.py
+++ app/__init__.py
@@ -1,18 +1,16 @@
-from flask import Flask, app
+from flask import Flask
 from .extensions import init_mongo
 from .routes.auth import auth_bp
 from .routes.landing import landing_bp
 from .routes.dashboard import dashboard_bp
-from .routes.auth import auth_bp
-app.register_blueprint(auth_bp)
 
 def create_app():
     app = Flask(__name__)
-    
-    # Load configs
+
+    # Load config from config.py
     app.config.from_pyfile("../config.py", silent=True)
 
-    # Initialize Mongo
+    # Initialize MongoDB
     init_mongo(app)
 
     # Register Blueprints
@@ -21,5 +19,3 @@ def create_app():
     app.register_blueprint(dashboard_bp)
 
     return app
-
-
